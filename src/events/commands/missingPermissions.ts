40d19c391d03a1a1c936d10182be3d3b68955199
DaangamesDG <dwklarenbeek@gmail.com>
2021-01-30 00:25:17 +0100

    added new command events

---
+++ src/events/commands/missingPermissions.ts
@@ -0,0 +1,20 @@
+import { Listener, Command } from "discord-akairo";
+import { PermissionString } from "discord.js";
+import { Message } from "discord.js";
+
+export default class missingPermissionsListener extends Listener {
+	constructor() {
+		super("missingPermissions", {
+			emitter: "commandHandler",
+			event: "missingPermissions",
+		});
+	}
+
+	exec(message: Message, command: Command, type: "client" | "user", missing: PermissionString[]) {
+		message.util.send(
+			`>>> 👮‍♂️ | Oops, **${type}** is missing the following permissions for \`${
+				command.id
+			}\`: \`${missing.join("`, `")}\`.`
+		);
+	}
+}
