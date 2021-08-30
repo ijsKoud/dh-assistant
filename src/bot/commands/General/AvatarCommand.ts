c07b7921eaa8f5d0049f8f241cb9554897b659a9
DaanGamesDG <daan@daangamesdg.wtf>
2021-08-30 16:35:38 +0200

    Feat: add AvatarCommand

---
+++ src/bot/commands/General/AvatarCommand.ts
@@ -0,0 +1,26 @@
+import { Command } from "../../../client/structures/Command";
+import { ApplyOptions } from "@sapphire/decorators";
+import { Message } from "discord.js";
+import { Args } from "@sapphire/framework";
+
+@ApplyOptions<Command.Options>({
+	name: "avatar",
+	aliases: ["avatar"],
+	description: "Shows the avatar of a user",
+	requiredClientPermissions: ["EMBED_LINKS"],
+})
+export default class AvatarCommand extends Command {
+	public async run(message: Message, args: Args): Promise<void> {
+		let { value: user } = await args.pickResult("user");
+		if (!user) user = message.author;
+
+		await message.reply({
+			embeds: [
+				this.container.client.utils
+					.embed()
+					.setTitle(`Avatar of ${user.tag}`)
+					.setImage(user.displayAvatarURL({ dynamic: true, size: 4096 })),
+			],
+		});
+	}
+}
