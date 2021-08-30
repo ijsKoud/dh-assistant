2f594d5590b5cab72e5fadb66969fa57bac4fd98
DaanGamesDG <daan@daangamesdg.wtf>
2021-08-30 17:29:55 +0200

    Feat: updating intents

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ config();
 import Client from "./client/Client";
 new Client({
 	owners: process.env.OWNERS?.split(" ") ?? [],
-	intents: ["GUILDS", "GUILD_MESSAGES"],
+	intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"],
 	debug: !!process.env.DEBUG,
 	partials: [],
 	activity: [
