aa06a2a6f4b01b3a01b1485e976a043927728c24
DaanGamesDG <daan@daangamesdg.wtf>
2021-08-30 17:00:18 +0200

    Feat: added Serverinfo command

---
+++ src/bot/commands/General/ServerinfoCommand.ts
@@ -0,0 +1,79 @@
+import { Command } from "../../../client/structures/Command";
+import { ApplyOptions } from "@sapphire/decorators";
+import { Message } from "discord.js";
+
+const levels: Record<"NONE" | "TIER_1" | "TIER_2" | "TIER_3", string> = {
+	NONE: "0",
+	TIER_1: "1",
+	TIER_2: "2",
+	TIER_3: "3",
+};
+
+@ApplyOptions<Command.Options>({
+	name: "serverinfo",
+	description: "Information about this bot",
+	requiredClientPermissions: ["EMBED_LINKS"],
+	preconditions: ["GuildOnly"],
+})
+export default class ServerinfoCommand extends Command {
+	public async run(message: Message): Promise<void> {
+		if (!message.guild) return;
+
+		const owner = await message.guild.fetchOwner();
+		await message.reply({
+			embeds: [
+				this.container.client.utils
+					.embed()
+					.setTitle(`Server Info: ${message.guild.name}`)
+					.setThumbnail(
+						message.guild.iconURL({ dynamic: true, size: 4096 }) ??
+							"https://cdn.daangamesdg.wtf/discord/wumpus.png"
+					)
+					.setFields([
+						{
+							name: "• Owner",
+							value: `**${owner.user.tag}**\n${owner.toString()}`,
+							inline: true,
+						},
+						{
+							name: "• Members",
+							value: `**Total**: ${message.guild.memberCount}\n**Users**: ${
+								message.guild.members.cache.filter((g) => !g.user.bot).size
+							}\n**Bots**: ${message.guild.members.cache.filter((g) => g.user.bot).size}`,
+							inline: true,
+						},
+						{
+							name: "• Channels",
+							value: `**Voice**: ${
+								message.guild.channels.cache.filter((c) => c.isVoice()).size
+							}\n**Text**: ${
+								message.guild.channels.cache.filter((c) => c.type === "GUILD_TEXT").size
+							}\n**Category**: ${
+								message.guild.channels.cache.filter((c) => c.type === "GUILD_CATEGORY").size
+							}`,
+							inline: true,
+						},
+						{
+							name: "• Roles",
+							value: `${this.container.client.utils
+								.trimArray(message.guild.roles.cache.map((r) => r.toString()).slice(1), 5)
+								.join(", ")}`,
+							inline: true,
+						},
+						{
+							name: "• Emojis & Stickers",
+							value: `**Stickers**: ${message.guild.stickers.cache.size}\n**Emojis**: ${message.guild.emojis.cache.size}`,
+							inline: true,
+						},
+						{
+							name: "• Boost status",
+							value: `**Boosts**: ${message.guild.premiumSubscriptionCount}\n**Level**: ${
+								levels[message.guild.premiumTier]
+							}`,
+							inline: true,
+						},
+					]),
+			],
+		});
+	}
+}
