7ecbb2b11854d69f248856f0d975aa4a573cb92d
DaanGamesDG <daan@daangamesdg.wtf>
2021-09-06 22:51:36 +0200

    Feat: added ModeratorOnly precondition

---
+++ src/bot/preconditions/permissions/ModeratorOnly.ts
@@ -0,0 +1,14 @@
+import { Precondition, PreconditionResult } from "@sapphire/framework";
+import { Message } from "discord.js";
+
+export class ModeratorOnlyPrecondition extends Precondition {
+	public run(message: Message): PreconditionResult {
+		if (!message.member) return this.ok();
+
+		return this.container.client.permissionHandler.hasMod(message.member)
+			? this.ok()
+			: this.error({
+					message: "Only **Moderators+** are allowed to use this command",
+			  });
+	}
+}
