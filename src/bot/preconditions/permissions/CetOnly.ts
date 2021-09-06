6991dc87b08ab9915474b44f52af949d5805b09c
DaanGamesDG <daan@daangamesdg.wtf>
2021-09-06 22:51:14 +0200

    Feat: added CetOnly precondition

---
+++ src/bot/preconditions/permissions/CetOnly.ts
@@ -0,0 +1,16 @@
+import { Precondition, PreconditionResult } from "@sapphire/framework";
+import { Message } from "discord.js";
+
+export class CetOnlyPrecondition extends Precondition {
+	public run(message: Message): PreconditionResult {
+		if (!message.member) return this.ok();
+
+		return this.container.client.permissionHandler.hasCet(message.member)
+			? this.ok()
+			: this.error({
+					message: `Only ${["Cet", "Manager", "Senior Team"]
+						.map((str) => `**${str}**`)
+						.join(", ")} are allowed to use this command`,
+			  });
+	}
+}
