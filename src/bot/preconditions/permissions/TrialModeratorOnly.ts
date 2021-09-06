834e884bc4f1a794a68e5b88b97508102def059c
DaanGamesDG <daan@daangamesdg.wtf>
2021-09-06 22:51:28 +0200

    Feat: added TrailModeratorOnly precondition

---
+++ src/bot/preconditions/permissions/TrialModeratorOnly.ts
@@ -0,0 +1,14 @@
+import { Precondition, PreconditionResult } from "@sapphire/framework";
+import { Message } from "discord.js";
+
+export class TrialModeratorOnlyPrecondition extends Precondition {
+	public run(message: Message): PreconditionResult {
+		if (!message.member) return this.ok();
+
+		return this.container.client.permissionHandler.hasTrial(message.member)
+			? this.ok()
+			: this.error({
+					message: "Only **Trial Moderators+** are allowed to use this command",
+			  });
+	}
+}
