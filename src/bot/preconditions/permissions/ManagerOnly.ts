503a02323e63657b831c8e50031ff009ecb8f61e
DaanGamesDG <daan@daangamesdg.wtf>
2021-09-06 22:51:45 +0200

    Feat: added ManagerOnly precondition

---
+++ src/bot/preconditions/permissions/ManagerOnly.ts
@@ -0,0 +1,14 @@
+import { Precondition, PreconditionResult } from "@sapphire/framework";
+import { Message } from "discord.js";
+
+export class ManagerOnlyPrecondition extends Precondition {
+	public run(message: Message): PreconditionResult {
+		if (!message.member) return this.ok();
+
+		return this.container.client.permissionHandler.hasManager(message.member)
+			? this.ok()
+			: this.error({
+					message: "Only **Managers+** are allowed to use this command",
+			  });
+	}
+}
