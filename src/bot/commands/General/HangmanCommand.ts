6b0cbf16f5ac62a039e9f54aeba4a350e5d0d2f4
DaanGamesDG <daan@daangamesdg.wtf>
2021-09-10 18:21:50 +0200

    Feat: added Hangman Command

---
+++ src/bot/commands/General/HangmanCommand.ts
@@ -0,0 +1,15 @@
+import { Command } from "../../../client/structures/extensions";
+import { ApplyOptions } from "@sapphire/decorators";
+import { Message } from "discord.js";
+import { hangMan } from "../../../client/structures/games";
+
+@ApplyOptions<Command.Options>({
+	name: "hangman",
+	description: "Starts a game of hangman",
+	requiredClientPermissions: ["EMBED_LINKS"],
+})
+export default class HangmanCommand extends Command {
+	public async run(message: Message) {
+		new hangMan(message).start();
+	}
+}
