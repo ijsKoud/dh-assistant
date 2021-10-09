22e17dd0588f1670731868c872fb0ce9f1b51854
DaanGamesDG <daan@daangamesdg.wtf>
2021-10-09 11:50:08 +0200

    fix(Utils): items cache check problem

--- src/client/Utils.ts
+++ src/client/Utils.ts
@@ -6,10 +6,7 @@ import {
 	Channel,
 	Collection,
 	DMChannel,
-	Emoji,
 	Guild,
-	GuildChannel,
-	GuildMember,
 	Interaction,
 	Message,
 	MessageActionRow,
@@ -20,8 +17,6 @@ import {
 	MessageEmbedOptions,
 	PermissionResolvable,
 	PermissionString,
-	Role,
-	User,
 } from "discord.js";
 import ms from "ms";
 import Client from "./Client";
@@ -92,72 +87,73 @@ export default class Utils {
 	}
 
 	public async getChannel(id: string): Promise<Channel | null> {
+		const resolve = () => {
+			const { cache } = this.client.channels;
+			return (
+				cache.get(id) ||
+				cache.find((channel) =>
+					"name" in channel
+						? (channel as Channel & { name: string }).name === id
+						: channel.toString() === id
+				)
+			);
+		};
+
 		return typeof id === "string"
-			? this._resolve(this.client.channels.cache, id) ||
-					(await this.client.channels.fetch(id).catch(() => null))
+			? resolve() || (await this.client.channels.fetch(id).catch(() => null))
 			: null;
 	}
 
 	public async getRole(id: string, guild: Guild) {
+		const resolve = () => {
+			const { cache } = guild.roles;
+			return cache.get(id) || cache.find((role) => role.name === id || role.toString() === id);
+		};
+
 		return typeof id === "string" && guild instanceof Guild
-			? this._resolve(guild.roles.cache, id) || (await guild.roles.fetch(id).catch(() => null))
+			? resolve() || (await guild.roles.fetch(id).catch(() => null))
 			: null;
 	}
 
 	public async fetchMember(id: string, guild: Guild | null | undefined) {
+		const resolve = () => {
+			if (!guild) return undefined;
+
+			const { cache } = guild.members;
+			return (
+				cache.get(id) ||
+				cache.find(
+					(member) =>
+						member.nickname === id ||
+						member.toString() === id ||
+						member.user.tag === id ||
+						member.user.username === id ||
+						member.user.toString() === id
+				)
+			);
+		};
+
 		return typeof id === "string" && guild instanceof Guild
-			? this._resolve(guild.members.cache, id) || (await guild.members.fetch(id).catch(() => null))
+			? resolve() || (await guild.members.fetch(id).catch(() => null))
 			: null;
 	}
 
 	public async fetchUser(id: string) {
-		return typeof id === "string"
-			? this._resolve(this.client.users.cache, id) ||
-					(await this.client.users.fetch(id).catch(() => null))
-			: null;
-	}
-
-	protected _resolve<T extends GuildMember | User | Channel | Guild | Emoji | Role>(
-		cache: Collection<string, T>,
-		id: string
-	): T | null {
-		const check = (item: T): boolean => {
-			let bool = false;
-			if (!(item instanceof Guild)) {
-				const reg = this._regex(item);
-				const match = id.match(reg);
-
-				bool = (bool || (match && match[1] === item.id)) ?? false;
-			}
-
-			if (!(item instanceof Channel && this.isDM(item))) {
-				const i = item as User | GuildChannel | Guild | Emoji | Role;
-				const name = (i instanceof User ? i.tag : i.name)?.toLowerCase();
-
-				if (name) bool = bool || name === id || name.includes(id);
-				else bool = bool ?? false;
-			}
-
-			return bool;
+		const resolve = () => {
+			const { cache } = this.client.users;
+			return (
+				cache.get(id) ||
+				cache.find((user) => user.tag === id || user.username === id || user.toString() === id)
+			);
 		};
 
-		return (
-			cache.get(id) ?? cache.find((v) => check(v)) ?? cache.find((v) => v.toString() === id) ?? null
-		);
+		return typeof id === "string"
+			? resolve() || (await this.client.users.fetch(id).catch(() => null))
+			: null;
 	}
 
 	public isDM(channel: Channel): channel is DMChannel {
 		return ["DM", "GROUP_DM"].includes(channel.type);
-	}
-
-	protected _regex(item: User | Channel | Emoji | Role | GuildMember): RegExp {
-		return item instanceof Emoji
-			? /<a?:[a-zA-Z0-9_]+:(\d{17,19})>/
-			: item instanceof Channel
-			? /<#(\d{17,19})>/
-			: item instanceof User || item instanceof GuildMember
-			? /<@!?(\d{17,19})>/
-			: /<@&(\d{17,19})>/;
 	}
 
 	public pagination(
