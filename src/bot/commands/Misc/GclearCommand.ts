12afdcc9bbf6e17e3fe21e9c349cb28258ccc7e7
DaanGamesDG <daan@daangamesdg.wtf>
2021-10-29 21:14:28 +0200

    feat: added gclear command

---
+++ src/bot/commands/Misc/GclearCommand.ts
@@ -0,0 +1,26 @@
+import { Command } from "../../../client/structures/extensions";
+import { ApplyOptions } from "@sapphire/decorators";
+import { GuildMessage } from "../../../client/structures/Moderation";
+import { emojis } from "../../../client/constants";
+
+@ApplyOptions<Command.Options>({
+	name: "gclear",
+	aliases: ["giveawayclear"],
+	description: "Clear the giveaway data",
+	preconditions: ["GuildOnly", "SeniorOnly"],
+})
+export default class GclearCommand extends Command {
+	public async messageRun(message: GuildMessage) {
+		let i = 0;
+		this.client.giveawaysManager.giveaways.forEach(async (giveaway) => {
+			if (giveaway.ended && giveaway.messageId) {
+				await this.client.giveawaysManager.deleteGiveaway(giveaway.messageId).catch(() => void 0);
+				i++;
+			}
+		});
+
+		await message.reply(
+			`>>> ${emojis.greentick} | I deleted **${i}** giveaways which wher for eligible deletion.`
+		);
+	}
+}
