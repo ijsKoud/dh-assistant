16937f1b19246eb384fa12cffaa30e005c6b3fee
DaanGamesDG <daan@daangamesdg.wtf>
2021-10-30 21:39:40 +0200

    feat: added MessageDelete event

---
+++ src/bot/listeners/message/MessageDelete.ts
@@ -0,0 +1,32 @@
+import { Listener, ListenerOptions } from "@sapphire/framework";
+import { ApplyOptions } from "@sapphire/decorators";
+import { Message } from "discord.js";
+
+@ApplyOptions<ListenerOptions>({ event: "messageDelete" })
+export default class MessageDeleteListener extends Listener {
+	public async run(message: Message) {
+		const { client } = this.container;
+		if (message.partial) return;
+		if (message.author.partial) await message.author.fetch();
+		if (
+			message.author.bot ||
+			message.guildId !== client.constants.guild ||
+			message.system ||
+			message.webhookId
+		)
+			return;
+
+		const embed = client.utils
+			.embed()
+			.setColor("#c04e3f")
+			.setTitle(`Message deleted in #${"name" in message.channel ? message.channel.name : ""}`)
+			.setDescription(message.content)
+			.setTimestamp()
+			.setFooter(
+				`${message.author.tag} - ${message.author.id}`,
+				message.author.displayAvatarURL({ dynamic: true, size: 4096 })
+			);
+
+		client.loggingHandler.sendLogs(embed, "message");
+	}
+}
