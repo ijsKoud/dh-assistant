5b1c75801b0160f2e0a842db6ae0d7f363d77b43
DaanGamesDG <daan@daangamesdg.wtf>
2021-11-07 11:42:46 +0100

    feat: adding directory checks

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import { config } from "dotenv";
+import { mkdir, readdir } from "fs/promises";
+import { join } from "path";
 config();
 
 import Client from "./client/Client";
-void new Client({
-	owners: process.env.OWNERS?.split(" ") ?? [],
-	intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "GUILD_PRESENCES", "GUILD_MEMBERS", "GUILD_BANS", "GUILD_MESSAGE_REACTIONS"],
-	debug: Boolean(process.env.DEBUG),
-	partials: ["CHANNEL", "GUILD_MEMBER", "MESSAGE", "USER"]
-}).start();
+
+(async () => {
+	const dirs = await readdir(process.cwd());
+	if (!dirs.includes("data")) await mkdir(join(process.cwd(), "data"));
+	if (!dirs.includes("transcripts")) await mkdir(join(process.cwd(), "transcripts"));
+
+	void new Client({
+		owners: process.env.OWNERS?.split(" ") ?? [],
+		intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "GUILD_PRESENCES", "GUILD_MEMBERS", "GUILD_BANS", "GUILD_MESSAGE_REACTIONS"],
+		debug: Boolean(process.env.DEBUG),
+		partials: ["CHANNEL", "GUILD_MEMBER", "MESSAGE", "USER"]
+	}).start();
+})();
